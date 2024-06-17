354df2b10f6e0882cdafab93cc9ac44b98d0f98d
Andy C <andy@oilshell.org>
2024-06-17 05:11:46 +0000

    Something is working on OpalStack

---
+++ opal.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Usage:
+#   ./opal.sh <function name>
+
+
+pip-install() {
+  set -x
+
+  # Activate OpalStack env template, which is Python 3.10
+
+  . ~/apps/hashdiv/env/bin/activate
+
+  python3 -m pip install -r requirements.txt
+}
+
+deploy() {
+  # It's just one file
+
+  cp -v hashdiv.py ~/apps/hashdiv/myapp
+}
+
+start() {
+  ~/apps/hashdiv/start
+}
+
+stop() {
+  ~/apps/hashdiv/stop
+}
+
+logs() {
+  tail -f ~/logs/apps/hashdiv/uwsgi.log
+}
+
+"$@"
